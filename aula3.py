cce1caaa6c50ecec8b0d88cd50510bdb8ab0d576
safira rodrigues <safirarodrigues.souza08@gmail.com>
2024-09-25 10:19:43 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,16 @@
+# variaveis em pyton
+# não há necessidade de declará -las no inicio do programa
+# basta criar no meio do algoritimo com a atribuição
+
+teste = "safira"
+print(teste) #imprime a string "safira"
+print(type(teste)) # imprime o tipo da variavel,string
+teste = 16
+print(teste) #imprime o numero 16
+print(type(teste)) #imprime o tipo da variavel, que é int
+teste = 1.64
+print(teste) #imprime o numero 1.64
+print(type(teste)) #imprime o tipo da variavel, que é float
+teste = True
+print(teste) # imprime o valor true 
+print(type(teste))#imprime o tipo da variavel, que é bool
